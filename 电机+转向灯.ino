514f01e5fcef45a411571603a1170e7fc5a17fc8
陆地小跑 <39906450+aibessie@users.noreply.github.com>
2019-07-04 10:03:11 +0800

    第二次作业（电机+转向灯）

---
+++ 电机+转向灯.ino
@@ -0,0 +1,107 @@
+int led1=12;
+int led2=2;
+int led3=8;
+int led4=7;
+
+void setup() {
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    Serial.begin(9600);
+}
+int income=0;
+
+void loop() {
+  if (Serial.available()>0)
+  {
+    income=Serial.read();
+    switch(income)
+    {
+      case'f':
+        forward();
+        break;
+      case 'b':
+        backward();
+        break;
+      case 'l':
+        left();
+        break;
+      case 'r':
+        right();
+        break;
+      case 's':
+        stop();
+        break;
+      default:
+             break;
+    }
+  }
+      
+        
+  }
+  
+    void forward()
+{
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+    
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,LOW);
+}
+
+void backward()
+{
+  digitalWrite(6,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+  
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,HIGH);
+  digitalWrite(led4,HIGH);
+}
+
+void left()
+{
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+  
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,HIGH);
+  digitalWrite(led4,LOW);
+}
+
+void right()
+{
+  digitalWrite(6,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+    
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,LOW);
+  digitalWrite(led4,HIGH);
+}
+
+void stop()
+{
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+   
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,HIGH);
+  digitalWrite(led4,HIGH);
+}
