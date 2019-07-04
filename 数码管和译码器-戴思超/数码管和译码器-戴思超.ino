59834396099e5381f2df8dcd91d30ff77676dc37
陆地小跑 <39906450+aibessie@users.noreply.github.com>
2019-07-04 16:24:49 +0800

    数码管和译码器-戴思超（第三次作业）

---
+++ 数码管和译码器-戴思超/数码管和译码器-戴思超.ino
@@ -0,0 +1,135 @@
+int income=0;
+void setup()
+{
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+   if (Serial.available()>=0)
+  {
+    income=Serial.read();
+    switch(income)
+    {
+      case'0':
+        zero();
+        break;
+      case '1':
+        one();
+        break;
+      case '2':
+        two();
+        break;
+      case '3':
+        three();
+        break;
+      case '4':
+        four();
+        break;
+      case'5':
+        five();
+        break;
+      case '6':
+        six();
+        break;
+      case '7':
+        seven();
+        break;
+      case '8':
+        eight();
+        break;
+      case '9':
+        nine();
+        break;
+      default:
+             break;
+    }
+  }
+      
+        
+  }
+void zero()
+{
+ digitalWrite(6,LOW);
+ digitalWrite(2,LOW);
+ digitalWrite(3,LOW);
+ digitalWrite(4,LOW);
+ digitalWrite(5,LOW);
+  }
+ void one()
+ {
+  digitalWrite(6,LOW);
+ digitalWrite(2,HIGH);
+ digitalWrite(3,LOW);
+ digitalWrite(4,LOW);
+ digitalWrite(5,LOW);
+ }
+void two()
+{
+ digitalWrite(6,LOW);
+ digitalWrite(2,LOW);
+ digitalWrite(3,HIGH);
+ digitalWrite(4,LOW);
+ digitalWrite(5,LOW);
+}
+void three()
+{digitalWrite(6,LOW);
+ digitalWrite(2,HIGH);
+ digitalWrite(3,HIGH);
+ digitalWrite(4,LOW);
+ digitalWrite(5,LOW);
+}
+void four()
+{
+  digitalWrite(6,LOW);
+ digitalWrite(2,LOW);
+ digitalWrite(3,LOW);
+ digitalWrite(4,HIGH);
+ digitalWrite(5,LOW);
+}
+void five()
+{
+ digitalWrite(6,LOW);
+ digitalWrite(2,HIGH);
+ digitalWrite(3,LOW);
+ digitalWrite(4,HIGH);
+ digitalWrite(5,LOW);
+}
+void six()
+{
+ digitalWrite(6,LOW);
+ digitalWrite(2,LOW);
+ digitalWrite(3,HIGH);
+ digitalWrite(4,HIGH);
+ digitalWrite(5,LOW);
+}
+void seven()
+{
+ digitalWrite(6,LOW);
+ digitalWrite(2,HIGH);
+ digitalWrite(3,HIGH);
+ digitalWrite(4,HIGH);
+ digitalWrite(5,LOW);
+}
+void eight()
+{
+  digitalWrite(6,LOW);
+ digitalWrite(2,LOW);
+ digitalWrite(3,LOW);
+ digitalWrite(4,LOW);
+ digitalWrite(5,HIGH);
+}
+void nine()
+{digitalWrite(6,LOW);
+ digitalWrite(2,HIGH);
+ digitalWrite(3,LOW);
+ digitalWrite(4,LOW);
+ digitalWrite(5,HIGH);
+}
+  
